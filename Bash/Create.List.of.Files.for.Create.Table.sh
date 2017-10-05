8c6473365edd54137abdc1618ca8f0d244d1743c
tAndreani <andreani.tommaso86@gmail.com>
2017-10-05 17:28:49 +0200

    Added a workflow for the scripts

--- Bash/Create.List.of.Files.for.Create.Table.sh
+++ Bash/Create.List.of.Files.for.Create.Table.sh
@@ -26,3 +26,84 @@ do
                  (cd $i ; wget -i $j); 
         done;
 done &
+
+
+#Unzip
+b=/project/jgu-cbdm/andradeLab/scratch/tandrean/Data/Jean-Fred/Test.New.ChIP/files/HD*/*bed.gz
+gunzip $b
+
+b=/project/jgu-cbdm/andradeLab/scratch/tandrean/Data/Jean-Fred/Test.New.ChIP/files/HD*/*bed
+
+for i in $b;
+do
+  awk '$9 >= 1.3' $i | sort -k1,1 -k2,2n > $i.q.val.thr.0.05.sort.bed; 
+done &
+
+
+c="CTCF/ENCFF002CEL.bed.q.val.thr.0.05.sort.bed 
+CTCF/ENCFF002DBD.bed.q.val.thr.0.05.sort.bed
+CTCF/ENCFF002DDJ.bed.q.val.thr.0.05.sort.bed
+CTCF/ENCFF738TKN.bed.q.val.thr.0.05.sort.bed
+EGR1/ENCFF004WYV.bed.q.val.thr.0.05.sort.bed
+EGR1/ENCFF558JBX.bed.q.val.thr.0.05.sort.bed
+HDAC2/ENCFF562JDH.bed.q.val.thr.0.05.sort.bed
+HDAC2/ENCFF657VJX.bed.q.val.thr.0.05.sort.bed
+KDM1A/ENCFF738WCE.bed.q.val.thr.0.05.sort.bed
+KDM1A/ENCFF838COI.bed.q.val.thr.0.05.sort.bed
+MNT/ENCFF059ONJ.bed.q.val.thr.0.05.sort.bed
+MNT/ENCFF567GSX.bed.q.val.thr.0.05.sort.bed
+MNT/ENCFF973OME.bed.q.val.thr.0.05.sort.bed
+NCOR1/ENCFF080NBZ.bed.q.val.thr.0.05.sort.bed
+NCOR1/ENCFF165BCW.bed.q.val.thr.0.05.sort.bed
+NCOR1/ENCFF366UBB.bed.q.val.thr.0.05.sort.bed
+POLR2A/ENCFF002CXQ.bed.q.val.thr.0.05.sort.bed
+POLR2A/ENCFF002CXR.bed.q.val.thr.0.05.sort.bed
+POLR2A/ENCFF248IWJ.bed.q.val.thr.0.05.sort.bed
+POLR2A/ENCFF937VZI.bed.q.val.thr.0.05.sort.bed
+RNF2/ENCFF644WLI.bed.q.val.thr.0.05.sort.bed
+RNF2/ENCFF972LPT.bed.q.val.thr.0.05.sort.bed
+SMARCA4/ENCFF002CVT.bed.q.val.thr.0.05.sort.bed
+SMARCA4/ENCFF197YHU.bed.q.val.thr.0.05.sort.bed
+SMARCA4/ENCFF883TOD.bed.q.val.thr.0.05.sort.bed
+TARDBP/ENCFF261XPP.bed.q.val.thr.0.05.sort.bed
+TARDBP/ENCFF905VXX.bed.q.val.thr.0.05.sort.bed"
+
+#Intersect regions overlapping
+for i in $c;
+do
+bedtools intersect -a GRCh38.Serial.Number.txt -b $i -u | awk '{$4=1}1'  | awk '{print $1"\t"$2"\t"$3"\t"$4}' > $i.intersected.GRCh38.400bp; 
+done 
+&
+
+#Intersect the regions non overlapping
+for i in $c;
+do
+bedtools intersect -a GRCh38.Serial.Number.txt -b $i -v | awk '{$4=0}1'  | awk '{print $1"\t"$2"\t"$3"\t"$4}' > $i.intersected.GRCh38.400bp.not; 
+done
+
+
+#Concatenate the Two Files
+for f in */*.400bp;
+do
+cat "$f" "$f.not" > "$f.yes.and.not" ;  
+done &
+
+#Sort the files
+for i in */*.yes.and.not;
+do
+sort -k 1,1 -k2,2n $i > $i.sorted;
+done &
+
+
+#Extract values 0= intersected and 1= intersected
+for i in */*.sorted;
+do
+cut -f 4 $i > $i.value;
+done &
+
+#
+d="CTCF EGR1 HDAC2 KDM1A MNT NCOR1 POLR2A RNF2 SMARCA4 TARDBP"
+for i in $d;
+do
+paste GRCh38.Serial.Number.txt $i/*.value | cut -f 1,2,3,5,6,7 > $i.txt;
+done
