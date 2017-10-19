4e259e1385def17cee53c1cb19171b88cde3e757
tAndreani <andreani.tommaso86@gmail.com>
2017-10-19 11:32:19 +0200

    Upgrade the scripts

--- R/Merge.Tables.r
+++ R/Merge.Tables.r
@@ -21,31 +21,7 @@ master.table <- Reduce(function(x, y) merge(x, y, all=TRUE), list(df1, df2, df3,
 colnames(master.table) <- c("Id","CTCF","EGR1","HDAC2","KDM1A","MNT","NCOR1","POLR2A","RNF2","SMARCA4")
 master.table[is.na(master.table)] <- 0
 master.table.matrix <- as.matrix(master.table[,2:10])
-head(master.table)
-
-master.table$sum <- rowSums(master.table[,2:10])
-cor.test(master.table$MNT,master.table$CTCF)
-hist(master.table$sum,xlim = c(1,9),main="Interacting Partner Histrogram",xlab = "Number of Proteins sharing a 400 bin", ylab = "Number of Bins")
+wirte.table(master.table,"master.table.txt",quote=FALSE,colnames=TRUE,rownames=TRUE,sep="\t")
 
 
-prot <- t(master.table.matrix)%*%master.table.matrix
-diag(prot) <- 0
-prot.scale <- scale(prot)
-head(prot.scale)
-
-library(gplots)
-heatmap.2(prot.scale,main = "Protein Bins Interactions",margins = c(5,5),reorderfun = function(d, w) reorder(d, w))
-
-set.seed(1)
-n <- 10
-replace=TRUE
-vec <- sample(master.table.matrix, replace=replace)
-dim(master.table.matrix)
-dim(vec) <- c(230826,9)
-colnames(vec) <- c("CTCF","EGR1","HDAC2","KDM1A","MNT","NCOR1","POLR2A","RNF2","SMARCA4")
-prot.samples <- t(vec)%*%vec
-diag(prot.samples) <- 0
-prot.samples.scale <- scale(prot.samples)
-head(prot.samples.scale)
-heatmap.2(prot.samples.scale,main = "Protein Bins Interactions Sampled",margins = c(5,5),reorderfun = function(d, w) reorder(d, w))
 
